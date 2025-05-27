33e910d57c2e1cba2952194d9b47143d04f4ea66
husseinadel <husseinadelhhh@gmail.com>
2025-05-28 00:41:44 +0300

    Adding Empty Interface and Class In BLL

---
+++ Carvo.Business_Logic_Layer/IServices/IEntityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carvo.Business_Logic_Layer.IServices
+{
+    internal class IEntityService
+    {
+    }
+}
---
+++ Carvo.Business_Logic_Layer/Services/EntityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carvo.Business_Logic_Layer.Services
+{
+    internal class EntityService
+    {
+    }
+}
