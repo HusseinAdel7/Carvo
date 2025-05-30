62e2f31a20d0296d96f9e2f4117b0fc619d39eac
hossam <hossam.222salama@gmail.com>
2025-05-30 20:03:43 +0300

    Modification In Mainform Adding Picture for Button and Styling of Line

--- Carvo.User_Interface_Layer/MainWindow.Designer.cs
+++ Carvo.User_Interface_Layer/MainWindow.Designer.cs
@@ -28,50 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
             button1 = new Button();
             button2 = new Button();
             sqlCommandBuilder1 = new Microsoft.Data.SqlClient.SqlCommandBuilder();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(990, 54);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = SystemColors.ButtonHighlight;
-            textBox1.Font = new Font("Segoe UI", 18F);
-            textBox1.Location = new Point(793, 30);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(344, 39);
-            textBox1.TabIndex = 1;
-            textBox1.Text = "شركة الاستاذ للاطارات والبطاريات";
-            // 
-            // textBox2
-            // 
-            textBox2.BackColor = SystemColors.ButtonFace;
-            textBox2.Font = new Font("Segoe UI", 18F);
-            textBox2.Location = new Point(235, 157);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(912, 39);
-            textBox2.TabIndex = 2;
-            textBox2.Text = "مرحباً بك في نظام إدارة الورشة يمكنك إدارة عمليات الصيانة والمخزون والفواتير بسهولة وأمان";
             // 
             // button1
             // 
             button1.BackColor = Color.FromArgb(244, 244, 244, 244);
             button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
-            button1.Location = new Point(507, 289);
+            button1.Location = new Point(298, 376);
             button1.Name = "button1";
-            button1.Size = new Size(126, 46);
+            button1.Size = new Size(140, 46);
             button1.TabIndex = 3;
             button1.Text = "موظف";
             button1.UseVisualStyleBackColor = false;
@@ -80,12 +54,55 @@
             // 
             button2.BackColor = Color.FromArgb(244, 244, 244, 244);
             button2.Font = new Font("Segoe UI", 18F);
-            button2.Location = new Point(1025, 285);
+            button2.Location = new Point(819, 376);
             button2.Name = "button2";
-            button2.Size = new Size(122, 50);
+            button2.Size = new Size(140, 50);
             button2.TabIndex = 4;
             button2.Text = "مدير";
             button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(48, 67, 87);
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(362, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(487, 45);
+            label1.TabIndex = 7;
+            label1.Text = "شركة الاستاذ للاطارات والبطاريات";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.software_engineer;
+            pictureBox1.Location = new Point(298, 215);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(140, 140);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.employee;
+            pictureBox2.Location = new Point(819, 215);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(140, 140);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(56, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(1128, 40);
+            label2.TabIndex = 10;
+            label2.Text = "مرحباً بك في نظام إدارة الورشة يمكنك إدارة عمليات الصيانة والمخزون والفواتير بسهولة وأمان";
             // 
             // MainWindow
             // 
@@ -93,26 +110,30 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(48, 67, 87);
             ClientSize = new Size(1370, 522);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
             Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
             Margin = new Padding(3, 2, 3, 2);
             Name = "MainWindow";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainWindow";
+            Load += MainWindow_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
         private Button button1;
         private Button button2;
         private Microsoft.Data.SqlClient.SqlCommandBuilder sqlCommandBuilder1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label2;
     }
 }
