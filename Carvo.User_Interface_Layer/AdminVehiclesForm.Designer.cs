6af2ef4191134763bd64895ebcdec3810c7507ea
Ali Gamal <aligamaldakhly99@gmail.com>
2025-05-31 01:09:10 +0300

    Added Admin Vehicles Form , Admin can add, update and delete vehicles for specific customer , insering vqhicle information using text boxes and dropdown list

---
+++ Carvo.User_Interface_Layer/AdminVehiclesForm.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace Carvo.User_Interface_Layer
+{
+    partial class AdminVehiclesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminVehiclesForm));
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            PrevImageAsBtn = new PictureBox();
+            CategoriesManagement = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            label4 = new Label();
+            CustomersDropdownList = new ComboBox();
+            VehicleNameTxt = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            UpdateVehicleBtn = new Button();
+            DeleteInvoiceImg = new PictureBox();
+            AddInvoiceImg = new PictureBox();
+            AddVehicleBtn = new Button();
+            DeleteVehicleBtn = new Button();
+            VehiclesGridView = new DataGridView();
+            PlateNumberTxt = new TextBox();
+            VehicleModelTxt = new TextBox();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PrevImageAsBtn).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DeleteInvoiceImg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)AddInvoiceImg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)VehiclesGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(VehiclesGridView, 0, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 133F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 438F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(1508, 829);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(48, 67, 87);
+            tableLayoutPanel1.SetColumnSpan(panel1, 2);
+            panel1.Controls.Add(PrevImageAsBtn);
+            panel1.Controls.Add(CategoriesManagement);
+            panel1.Dock = DockStyle.Fill;
+            panel1.ForeColor = Color.White;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1502, 127);
+            panel1.TabIndex = 0;
+            // 
+            // PrevImageAsBtn
+            // 
+            PrevImageAsBtn.Image = (Image)resources.GetObject("PrevImageAsBtn.Image");
+            PrevImageAsBtn.Location = new Point(42, 20);
+            PrevImageAsBtn.Name = "PrevImageAsBtn";
+            PrevImageAsBtn.Size = new Size(50, 50);
+            PrevImageAsBtn.SizeMode = PictureBoxSizeMode.StretchImage;
+            PrevImageAsBtn.TabIndex = 1;
+            PrevImageAsBtn.TabStop = false;
+            // 
+            // CategoriesManagement
+            // 
+            CategoriesManagement.AutoSize = true;
+            CategoriesManagement.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            CategoriesManagement.Location = new Point(707, 35);
+            CategoriesManagement.Name = "CategoriesManagement";
+            CategoriesManagement.Size = new Size(175, 38);
+            CategoriesManagement.TabIndex = 0;
+            CategoriesManagement.Text = "ادارة المركبات";
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.None;
+            panel2.BackColor = Color.FromArgb(48, 67, 87);
+            panel2.Controls.Add(VehicleModelTxt);
+            panel2.Controls.Add(PlateNumberTxt);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(CustomersDropdownList);
+            panel2.Controls.Add(VehicleNameTxt);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(UpdateVehicleBtn);
+            panel2.Controls.Add(DeleteInvoiceImg);
+            panel2.Controls.Add(AddInvoiceImg);
+            panel2.Controls.Add(AddVehicleBtn);
+            panel2.Controls.Add(DeleteVehicleBtn);
+            panel2.Location = new Point(60, 136);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1388, 432);
+            panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(515, 53);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 25);
+            label3.TabIndex = 17;
+            label3.Text = " : الموديل ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(510, 126);
+            label4.Name = "label4";
+            label4.Size = new Size(119, 25);
+            label4.TabIndex = 15;
+            label4.Text = " : اسم العميل ";
+            label4.TextAlign = ContentAlignment.TopRight;
+            // 
+            // CustomersDropdownList
+            // 
+            CustomersDropdownList.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            CustomersDropdownList.FormattingEnabled = true;
+            CustomersDropdownList.Location = new Point(177, 130);
+            CustomersDropdownList.Name = "CustomersDropdownList";
+            CustomersDropdownList.Size = new Size(281, 31);
+            CustomersDropdownList.TabIndex = 12;
+            // 
+            // VehicleNameTxt
+            // 
+            VehicleNameTxt.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            VehicleNameTxt.Location = new Point(796, 51);
+            VehicleNameTxt.Multiline = true;
+            VehicleNameTxt.Name = "VehicleNameTxt";
+            VehicleNameTxt.Size = new Size(373, 39);
+            VehicleNameTxt.TabIndex = 9;
+            VehicleNameTxt.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(1233, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 25);
+            label2.TabIndex = 8;
+            label2.Text = " : رقم اللوحة ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(1222, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 25);
+            label1.TabIndex = 7;
+            label1.Text = " : اسم المركبة ";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(712, 290);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(64, 52);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // UpdateVehicleBtn
+            // 
+            UpdateVehicleBtn.BackColor = Color.FromArgb(224, 224, 224);
+            UpdateVehicleBtn.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            UpdateVehicleBtn.Location = new Point(576, 290);
+            UpdateVehicleBtn.Name = "UpdateVehicleBtn";
+            UpdateVehicleBtn.Size = new Size(130, 52);
+            UpdateVehicleBtn.TabIndex = 5;
+            UpdateVehicleBtn.Text = "تحديث مركبة";
+            UpdateVehicleBtn.UseVisualStyleBackColor = false;
+            // 
+            // DeleteInvoiceImg
+            // 
+            DeleteInvoiceImg.Image = (Image)resources.GetObject("DeleteInvoiceImg.Image");
+            DeleteInvoiceImg.Location = new Point(327, 290);
+            DeleteInvoiceImg.Name = "DeleteInvoiceImg";
+            DeleteInvoiceImg.Size = new Size(64, 52);
+            DeleteInvoiceImg.SizeMode = PictureBoxSizeMode.StretchImage;
+            DeleteInvoiceImg.TabIndex = 4;
+            DeleteInvoiceImg.TabStop = false;
+            // 
+            // AddInvoiceImg
+            // 
+            AddInvoiceImg.Image = (Image)resources.GetObject("AddInvoiceImg.Image");
+            AddInvoiceImg.Location = new Point(1102, 290);
+            AddInvoiceImg.Name = "AddInvoiceImg";
+            AddInvoiceImg.Size = new Size(64, 52);
+            AddInvoiceImg.SizeMode = PictureBoxSizeMode.StretchImage;
+            AddInvoiceImg.TabIndex = 3;
+            AddInvoiceImg.TabStop = false;
+            // 
+            // AddVehicleBtn
+            // 
+            AddVehicleBtn.BackColor = Color.FromArgb(224, 224, 224);
+            AddVehicleBtn.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            AddVehicleBtn.Location = new Point(966, 290);
+            AddVehicleBtn.Name = "AddVehicleBtn";
+            AddVehicleBtn.Size = new Size(130, 52);
+            AddVehicleBtn.TabIndex = 2;
+            AddVehicleBtn.Text = "اضافة مركبة ";
+            AddVehicleBtn.UseVisualStyleBackColor = false;
+            // 
+            // DeleteVehicleBtn
+            // 
+            DeleteVehicleBtn.BackColor = Color.FromArgb(224, 224, 224);
+            DeleteVehicleBtn.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            DeleteVehicleBtn.Location = new Point(191, 290);
+            DeleteVehicleBtn.Name = "DeleteVehicleBtn";
+            DeleteVehicleBtn.Size = new Size(130, 52);
+            DeleteVehicleBtn.TabIndex = 1;
+            DeleteVehicleBtn.Text = "مسح مركبة";
+            DeleteVehicleBtn.UseVisualStyleBackColor = false;
+            // 
+            // VehiclesGridView
+            // 
+            VehiclesGridView.AllowUserToAddRows = false;
+            dataGridViewCellStyle6.BackColor = SystemColors.ButtonFace;
+            dataGridViewCellStyle6.ForeColor = Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = Color.CornflowerBlue;
+            VehiclesGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            VehiclesGridView.Anchor = AnchorStyles.None;
+            VehiclesGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            VehiclesGridView.BorderStyle = BorderStyle.None;
+            VehiclesGridView.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle7.BackColor = Color.FromArgb(48, 67, 87);
+            dataGridViewCellStyle7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle7.ForeColor = Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            VehiclesGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            VehiclesGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = Color.White;
+            dataGridViewCellStyle8.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle8.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle8.Padding = new Padding(0, 2, 2, 2);
+            dataGridViewCellStyle8.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.False;
+            VehiclesGridView.DefaultCellStyle = dataGridViewCellStyle8;
+            VehiclesGridView.EnableHeadersVisualStyles = false;
+            VehiclesGridView.Location = new Point(60, 583);
+            VehiclesGridView.MaximumSize = new Size(1387, 285);
+            VehiclesGridView.Name = "VehiclesGridView";
+            VehiclesGridView.ReadOnly = true;
+            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle9.BackColor = SystemColors.Control;
+            dataGridViewCellStyle9.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle9.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = DataGridViewTriState.True;
+            VehiclesGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            VehiclesGridView.RowHeadersVisible = false;
+            VehiclesGridView.RowHeadersWidth = 51;
+            dataGridViewCellStyle10.Alignment = DataGridViewContentAlignment.MiddleRight;
+            VehiclesGridView.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            VehiclesGridView.ScrollBars = ScrollBars.Vertical;
+            VehiclesGridView.Size = new Size(1387, 234);
+            VehiclesGridView.TabIndex = 2;
+            VehiclesGridView.DataBindingComplete += InvoicesGridView_DataBindingComplete;
+            // 
+            // PlateNumberTxt
+            // 
+            PlateNumberTxt.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PlateNumberTxt.Location = new Point(796, 129);
+            PlateNumberTxt.Multiline = true;
+            PlateNumberTxt.Name = "PlateNumberTxt";
+            PlateNumberTxt.Size = new Size(373, 39);
+            PlateNumberTxt.TabIndex = 18;
+            PlateNumberTxt.TextAlign = HorizontalAlignment.Right;
+            // 
+            // VehicleModelTxt
+            // 
+            VehicleModelTxt.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            VehicleModelTxt.Location = new Point(85, 52);
+            VehicleModelTxt.Multiline = true;
+            VehicleModelTxt.Name = "VehicleModelTxt";
+            VehicleModelTxt.Size = new Size(373, 39);
+            VehicleModelTxt.TabIndex = 19;
+            VehicleModelTxt.TextAlign = HorizontalAlignment.Right;
+            // 
+            // AdminProductsForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(1508, 829);
+            Controls.Add(tableLayoutPanel1);
+            MaximumSize = new Size(1526, 885);
+            Name = "AdminProductsForm";
+            Text = "AdminInvoicesForm";
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)PrevImageAsBtn).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DeleteInvoiceImg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)AddInvoiceImg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)VehiclesGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private Label CategoriesManagement;
+        private Panel panel2;
+        private Button AddVehicleBtn;
+        private Button DeleteVehicleBtn;
+        private DataGridView VehiclesGridView;
+        private PictureBox DeleteInvoiceImg;
+        private PictureBox AddInvoiceImg;
+        private PictureBox PrevImageAsBtn;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button UpdateVehicleBtn;
+        private TextBox VehicleNameTxt;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox CustomersDropdownList;
+        private TextBox VehicleModelTxt;
+        private TextBox PlateNumberTxt;
+    }
+}
--- Carvo.User_Interface_Layer/Program.cs
+++ Carvo.User_Interface_Layer/Program.cs
@@ -30,7 +30,7 @@ namespace Carvo.User_Interface_Layer
                      services.AddTransient<SalesInvoiceForm>();
                      services.AddTransient<RepairInvoiceForm>();
                      services.AddTransient<DashboardForm>();
-
+                     services.AddTransient<AdminVehiclesForm>();
 
                  })
                  .Build();
@@ -68,8 +68,10 @@ namespace Carvo.User_Interface_Layer
             var repairInvoiceFrom = host.Services.GetRequiredService<RepairInvoiceForm>();
             var salesInvoiceFrom = host.Services.GetRequiredService<SalesInvoiceForm>();
             var dashboardForm = host.Services.GetRequiredService<DashboardForm>();
+            var adminVehiclesForm = host.Services.GetRequiredService<AdminVehiclesForm>();
 
-            Application.Run(dashboardForm);
+
+            Application.Run(adminVehiclesForm);
         }
     }
 }
