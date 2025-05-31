a9332839c2011758e2ecca9a76c986d68605aefa
mahmoud kamal <mahmoudkyassin88@gmail.com>
2025-05-31 18:23:15 +0300

    changed picture - label position from left to right and made them bold - removed role label - added close and minimize button icons - changed form startup default position

--- Carvo.User_Interface_Layer/LoginForm.cs
+++ Carvo.User_Interface_Layer/LoginForm.cs
@@ -37,12 +37,12 @@ namespace Carvo.User_Interface_Layer
             }
         }
 
-        private void closeBtn_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private void minimizeBtn_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
         }
