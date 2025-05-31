433e38fb85d1bab7e7def02ab0cc6c5553cfdb6d
Ali Gamal <aligamaldakhly99@gmail.com>
2025-05-31 12:51:00 +0300

    Added Admin Suppliers Form , you can add delete update any supplier.

---
+++ Carvo.User_Interface_Layer/AdminSuppliersForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Carvo.Data_Access_Layer.Entities;
+
+namespace Carvo.User_Interface_Layer
+{
+    public partial class AdminSuppliersForm : Form
+    {
+        public AdminSuppliersForm()
+        {
+            InitializeComponent();
+
+            /* Testing Grid View */
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+                new Product() { Id = 1, Description = "vdsfvdsv", Name = "vfdfv", Price = 2551 },
+
+            };
+
+            var GridView = products.Select(p => new { Name = p.Name, Description = p.Description, Price = p.Price }).ToList();
+
+            SuppliersGridView.AllowUserToAddRows = false;
+            SuppliersGridView.DataSource = GridView;
+
+            SuppliersGridView.Columns[0].HeaderText = "الاسم";
+            SuppliersGridView.Columns[1].HeaderText = "الوصف";
+            SuppliersGridView.Columns[2].HeaderText = "سعر الوحدة";
+
+        }
+
+        private void InvoicesGridView_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            ResizeDataGridViewHeight();
+        }
+
+        private void ResizeDataGridViewHeight()
+        {
+            int totalHeight = SuppliersGridView.ColumnHeadersHeight;
+
+            foreach (DataGridViewRow row in SuppliersGridView.Rows)
+            {
+                if (row.Visible) totalHeight += row.Height;
+            }
+
+            SuppliersGridView.Height = totalHeight + 2; // 2px padding
+        }
+    }
+}
