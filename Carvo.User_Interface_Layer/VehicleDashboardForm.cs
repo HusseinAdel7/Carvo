4c5adfe97448ca19d5bd88665971bfcde87f85b0
mahmoud kamal <mahmoudkyassin88@gmail.com>
2025-06-01 01:08:39 +0300

    changed background color - added pannels to help orgainzing form controles - changed close and minimize button icons - added goBack and logOut buttons - changed the style of dataGridView

---
+++ Carvo.User_Interface_Layer/VehicleDashboardForm.cs
@@ -0,0 +1,73 @@
+﻿using Carvo.User_Interface_Layer.UIHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carvo.User_Interface_Layer
+{
+    public partial class VehicleDashboardForm : Form
+    {
+        public VehicleDashboardForm()
+        {
+            InitializeComponent();
+            HoverHelper.ApplyHoverToAllButtons(this); // Apply hover effects to all buttons in this form
+        }
+
+        private void CustomerDashboardForm_Load(object sender, EventArgs e)
+        {
+            UIHelper.SetupDataGridView(vehicleGrid);
+
+            // apply rounded corners to the form
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox || ctrl is Button)
+                {
+                    UIHelper.MakeControlRounded(ctrl, 16); // degree of rounding
+                }
+            }
+
+            // dummy temporary data for testing
+            vehicleGrid.Columns.Add("Id", "مسلسل");
+            vehicleGrid.Columns.Add("Name", "اسم المركبة");
+            vehicleGrid.Columns.Add("Model", "موديل المركبة");
+            vehicleGrid.Columns.Add("PlateNumber", "رقم اللوحة");
+
+            vehicleGrid.Rows.Add(1, "لانسر", "بوما 2008", "2512");
+            vehicleGrid.Rows.Add(2, "تويوتا", "كورولا 2015", "1234");
+            vehicleGrid.Rows.Add(3, "هوندا", "أكورد 2020", "5678");
+            vehicleGrid.Rows.Add(4, "فورد", "إكسبلورر 2019", "9101");
+            vehicleGrid.Rows.Add(5, "نيسان", "باترول 2021", "1122");
+            vehicleGrid.Rows.Add(6, "كيا", "سبورتاج 2018", "3344");
+            vehicleGrid.Rows.Add(7, "هيونداي", "سوناتا 2017", "5566");
+            vehicleGrid.Rows.Add(8, "شيفروليه", "ماليبو 2016", "7788");
+            vehicleGrid.Rows.Add(9, "مرسيدس", "E-Class 2022", "9900");
+
+
+            // to icrease the height of the DataGridView to fit all rows
+            vehicleGrid.Height = vehicleGrid.Rows.GetRowsHeight(DataGridViewElementStates.Visible)
+                                  + vehicleGrid.ColumnHeadersHeight;
+
+            // to set the form height to fit the DataGridView and add some padding
+            this.Height = vehicleGrid.Bottom + 50; // empty space at the bottom
+
+
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
