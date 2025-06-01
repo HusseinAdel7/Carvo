62b63e6fa01b1bd90b11aa7904ab6b07566d951e
abdelrahmanabdelsabour <abdoabdelsabour355@gmail.com>
2025-06-01 10:50:45 +0200

    change size and color of kabels

--- Carvo.User_Interface_Layer/DashboardForm.Designer.cs
+++ Carvo.User_Interface_Layer/DashboardForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardForm));
             panel1 = new Panel();
-            pictureBox1 = new PictureBox();
-            label1 = new Label();
             label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
             pictureBox4 = new PictureBox();
@@ -85,6 +85,29 @@
             panel1.Size = new Size(357, 193);
             panel1.TabIndex = 0;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(48, 67, 87);
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(145, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 21);
+            label2.TabIndex = 2;
+            label2.Text = "العملاء";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(126, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 21);
+            label1.TabIndex = 1;
+            label1.Text = "عدد العملاء : 120";
+            // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
@@ -94,24 +117,6 @@
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(126, 164);
-            label1.Name = "label1";
-            label1.Size = new Size(84, 15);
-            label1.TabIndex = 1;
-            label1.Text = "عدد العملاء : 120";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(145, 131);
-            label2.Name = "label2";
-            label2.Size = new Size(40, 15);
-            label2.TabIndex = 2;
-            label2.Text = "العملاء";
             // 
             // pictureBox2
             // 
@@ -166,18 +171,22 @@
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
             label3.Location = new Point(145, 131);
             label3.Name = "label3";
-            label3.Size = new Size(49, 15);
+            label3.Size = new Size(66, 21);
             label3.TabIndex = 2;
             label3.Text = "الموردين";
             // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
             label4.Location = new Point(126, 164);
             label4.Name = "label4";
-            label4.Size = new Size(93, 15);
+            label4.Size = new Size(125, 21);
             label4.TabIndex = 1;
             label4.Text = "عدد الموردين : 120";
             // 
@@ -204,18 +213,22 @@
             // label5
             // 
             label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
             label5.Location = new Point(145, 131);
             label5.Name = "label5";
-            label5.Size = new Size(42, 15);
+            label5.Size = new Size(56, 21);
             label5.TabIndex = 2;
             label5.Text = "الفواتير";
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
             label6.Location = new Point(126, 164);
             label6.Name = "label6";
-            label6.Size = new Size(86, 15);
+            label6.Size = new Size(115, 21);
             label6.TabIndex = 1;
             label6.Text = "عدد الفواتير : 120";
             // 
@@ -242,18 +255,22 @@
             // label7
             // 
             label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
             label7.Location = new Point(145, 131);
             label7.Name = "label7";
-            label7.Size = new Size(55, 15);
+            label7.Size = new Size(73, 21);
             label7.TabIndex = 2;
             label7.Text = "الموظفين";
             // 
             // label8
             // 
             label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.White;
             label8.Location = new Point(126, 164);
             label8.Name = "label8";
-            label8.Size = new Size(99, 15);
+            label8.Size = new Size(132, 21);
             label8.TabIndex = 1;
             label8.Text = "عدد الموظفين : 120";
             // 
@@ -280,18 +297,22 @@
             // label9
             // 
             label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.White;
             label9.Location = new Point(145, 131);
             label9.Name = "label9";
-            label9.Size = new Size(58, 15);
+            label9.Size = new Size(77, 21);
             label9.TabIndex = 2;
             label9.Text = "التصنيفات";
             // 
             // label10
             // 
             label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.White;
             label10.Location = new Point(126, 164);
             label10.Name = "label10";
-            label10.Size = new Size(102, 15);
+            label10.Size = new Size(136, 21);
             label10.TabIndex = 1;
             label10.Text = "عدد التصنيفات : 120";
             // 
@@ -318,18 +339,22 @@
             // label11
             // 
             label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.White;
             label11.Location = new Point(145, 131);
             label11.Name = "label11";
-            label11.Size = new Size(50, 15);
+            label11.Size = new Size(66, 21);
             label11.TabIndex = 2;
             label11.Text = "المنتجات";
             // 
             // label12
             // 
             label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.White;
             label12.Location = new Point(126, 164);
             label12.Name = "label12";
-            label12.Size = new Size(94, 15);
+            label12.Size = new Size(125, 21);
             label12.TabIndex = 1;
             label12.Text = "عدد المنتجات : 120";
             // 
