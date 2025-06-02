541140dce475b45fd0698497595666bf84782385
Ali Gamal <aligamaldakhly99@gmail.com>
2025-06-02 03:17:06 +0300

    Added The rest of IServices and Services in Bussiness Layer, tested  the Read operation of Product Service and Worked Successfully.

---
+++ Carvo.Business_Logic_Layer/Services/InvoiceService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carvo.Business_Logic_Layer.IServices;
+using Carvo.Data_Access_Layer.Entities;
+using Carvo.Data_Access_Layer.Repository.IGenericRepositories;
+
+namespace Carvo.Business_Logic_Layer.Services
+{
+    public class InvoiceService: IInvoiceService
+    {
+        private readonly IGenericRepository<Invoice, int> _invoiceRepository;
+
+        public InvoiceService(IGenericRepository<Invoice, int> invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+
+        public Task<IEnumerable<Invoice>> GetAllInvoicesAsync()
+        {
+            return _invoiceRepository.GetAllAsync();
+        }
+
+        public Task<Invoice?> GetInvoiceByIdAsync(int id)
+        {
+            return _invoiceRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Invoice> AddInvoiceAsync(Invoice invoice)
+        {
+            if (invoice == null)
+                throw new ArgumentNullException(nameof(invoice));
+
+            invoice.InvoiceDate = DateTime.UtcNow;
+            var addedInvoice = await _invoiceRepository.AddAsync(invoice);
+            return addedInvoice;
+        }
+
+        public async Task<Invoice> UpdateInvoiceAsync(Invoice invoice)
+        {
+            if (invoice == null)
+                throw new ArgumentNullException(nameof(invoice));
+
+            var existingInvoice = await _invoiceRepository.GetByIdAsync(invoice.Id);
+            if (existingInvoice == null)
+                throw new KeyNotFoundException($"Invoice with ID {invoice.Id} not found.");
+
+            // Update necessary fields
+            
+            existingInvoice.InvoiceNumber = invoice.InvoiceNumber;
+            existingInvoice.RepairAmount = invoice.RepairAmount;
+            existingInvoice.SaleAmount = invoice.SaleAmount;
+            existingInvoice.InvoiceType = invoice.InvoiceType;
+            existingInvoice.CustomerId = invoice.CustomerId;
+            existingInvoice.UserId = invoice.UserId;
+
+
+            var updatedInvoice = await _invoiceRepository.UpdateAsync(existingInvoice);
+            return updatedInvoice;
+        }
+
+        public async Task<bool> DeleteInvoiceAsync(int id)
+        {
+            return await _invoiceRepository.DeleteAsync(id);
+        }
+    }
+}
---
+++ Carvo.Business_Logic_Layer/Services/SupplierService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carvo.Business_Logic_Layer.IServices;
+using Carvo.Data_Access_Layer.Entities.Users;
+using Carvo.Data_Access_Layer.Repository.IGenericRepositories;
+
+namespace Carvo.Business_Logic_Layer.Services
+{
+    public class SupplierService : ISupplierService
+    {
+        private readonly IGenericRepository<Supplier, int> _supplierRepository;
+
+        public SupplierService(IGenericRepository<Supplier, int> supplierRepository)
+        {
+            _supplierRepository = supplierRepository;
+        }
+
+        public Task<IEnumerable<Supplier>> GetAllSuppliersAsync()
+        {
+            return _supplierRepository.GetAllAsync();
+        }
+
+        public Task<Supplier?> GetSupplierByIdAsync(int id)
+        {
+            return _supplierRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Supplier> AddSupplierAsync(Supplier supplier)
+        {
+            if (supplier == null)
+                throw new ArgumentNullException(nameof(supplier));
+
+            supplier.CreatedAt = DateTime.UtcNow;
+            var addedSupplier = await _supplierRepository.AddAsync(supplier);
+            return addedSupplier;
+        }
+
+        public async Task<Supplier> UpdateSupplierAsync(Supplier supplier)
+        {
+            if (supplier == null)
+                throw new ArgumentNullException(nameof(supplier));
+
+            var existingSupplier = await _supplierRepository.GetByIdAsync(supplier.Id);
+            if (existingSupplier == null)
+                throw new KeyNotFoundException($"Supplier with ID {supplier.Id} not found.");
+
+            existingSupplier.Name = supplier.Name;
+            existingSupplier.PhoneNumber = supplier.PhoneNumber;
+            existingSupplier.Address = supplier.Address;
+            existingSupplier.ComapayName = supplier.ComapayName;
+            //existingSupplier.NationalId = supplier.NationalId;
+          
+
+
+            var updatedSupplier = await _supplierRepository.UpdateAsync(existingSupplier);
+            return updatedSupplier;
+        }
+
+        public async Task<bool> DeleteSupplierAsync(int id)
+        {
+            return await _supplierRepository.DeleteAsync(id);
+        }
+    }
+}
