6f36ab861c5b20a55441e5e338ab2fe5b34b76ca
Hajar165 <learning1652@gmail.com>
2025-06-04 10:11:35 +0300

    Update Invioce Repaire  Design

--- Carvo.User_Interface_Layer/RepairInvoiceForm.Designer.cs
+++ Carvo.User_Interface_Layer/RepairInvoiceForm.Designer.cs
@@ -43,13 +43,32 @@
             button3 = new Button();
             button2 = new Button();
             label1 = new Label();
+            button6 = new Button();
+            dataGridView1 = new DataGridView();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            button7 = new Button();
+            button8 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            label2 = new Label();
             panel2.SuspendLayout();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // panel2
             // 
-            panel2.BackColor = Color.FromArgb(244, 244, 244);
+            panel2.BackColor = Color.FromArgb(48, 67, 87);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(numericUpDown1);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(button7);
+            panel2.Controls.Add(button8);
+            panel2.Controls.Add(button6);
             panel2.Controls.Add(label6);
             panel2.Controls.Add(label5);
             panel2.Controls.Add(button1);
@@ -57,10 +76,9 @@
             panel2.Controls.Add(label3);
             panel2.Controls.Add(comboBox2);
             panel2.Controls.Add(comboBox1);
-            panel2.Dock = DockStyle.Bottom;
-            panel2.Location = new Point(0, 116);
+            panel2.Location = new Point(0, 129);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1508, 254);
+            panel2.Size = new Size(1508, 190);
             panel2.TabIndex = 6;
             // 
             // label6
@@ -81,7 +99,7 @@
             label5.BackColor = Color.FromArgb(244, 244, 244);
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 178);
             label5.ForeColor = Color.FromArgb(48, 67, 87);
-            label5.Location = new Point(559, 78);
+            label5.Location = new Point(683, 78);
             label5.Name = "label5";
             label5.Size = new Size(81, 28);
             label5.TabIndex = 9;
@@ -93,7 +111,7 @@
             button1.FlatAppearance.BorderSize = 0;
             button1.FlatStyle = FlatStyle.Flat;
             button1.Image = (Image)resources.GetObject("button1.Image");
-            button1.Location = new Point(1404, 3);
+            button1.Location = new Point(1406, 4);
             button1.Name = "button1";
             button1.Size = new Size(50, 50);
             button1.TabIndex = 1;
@@ -129,9 +147,9 @@
             comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "عربية ", "موتوسيكل ", "عجلة" });
-            comboBox2.Location = new Point(1026, 82);
+            comboBox2.Location = new Point(909, 82);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(151, 28);
+            comboBox2.Size = new Size(268, 28);
             comboBox2.TabIndex = 1;
             // 
             // comboBox1
@@ -140,9 +158,9 @@
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Customer1", "Customer2", "Customer3", "Customer4", "Customer5", "" });
-            comboBox1.Location = new Point(237, 78);
+            comboBox1.Location = new Point(365, 78);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(153, 28);
+            comboBox1.Size = new Size(253, 28);
             comboBox1.TabIndex = 0;
             // 
             // panel1
@@ -218,11 +236,91 @@
             label1.Text = "فاتورة صيانة ";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            button6.ForeColor = Color.FromArgb(48, 67, 87);
+            button6.Location = new Point(1264, 4);
+            button6.Name = "button6";
+            button6.Size = new Size(132, 50);
+            button6.TabIndex = 11;
+            button6.Text = "استخراج الفاتورة";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 328);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1508, 267);
+            dataGridView1.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(149, 18);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(50, 50);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 16;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(140, 123);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(50, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(244, 244, 244);
+            button7.Location = new Point(22, 123);
+            button7.Name = "button7";
+            button7.Size = new Size(112, 50);
+            button7.TabIndex = 14;
+            button7.Text = "تحديث";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(244, 244, 244);
+            button8.Location = new Point(22, 18);
+            button8.Name = "button8";
+            button8.Size = new Size(112, 50);
+            button8.TabIndex = 13;
+            button8.Text = "إضافة";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(1027, 146);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(244, 244, 244);
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 178);
+            label2.ForeColor = Color.FromArgb(48, 67, 87);
+            label2.Location = new Point(1277, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 28);
+            label2.TabIndex = 18;
+            label2.Text = "سعر التصليح";
+            // 
             // RepairInvoiceForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1508, 370);
+            ClientSize = new Size(1508, 594);
+            Controls.Add(dataGridView1);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
@@ -236,6 +334,10 @@
             panel2.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
         }
 
@@ -255,5 +357,13 @@
         private Button button3;
         private Button button5;
         private Button button4;
+        private Button button6;
+        private DataGridView dataGridView1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Button button7;
+        private Button button8;
+        private NumericUpDown numericUpDown1;
+        private Label label2;
     }
 }
